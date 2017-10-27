ad94cccdde384aec92cf6c2a807516946608c9ec
James King <james.king@facepunchstudios.com>
2017-10-27 23:07:02 +0100

    Started work on scripting

---
+++ js/replayviewer.js
@@ -0,0 +1,18 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+///<reference path="../js/facepunch.webgame.d.ts"/>
+///<reference path="../js/sourceutils.d.ts"/>
+var WebGame = Facepunch.WebGame;
+var ReplayViewer = (function (_super) {
+    __extends(ReplayViewer, _super);
+    function ReplayViewer() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ReplayViewer.prototype.onInitialize = function () {
+        this.useDefaultCameraControl = false;
+    };
+    return ReplayViewer;
+}(SourceUtils.MapViewer));
---
+++ src/ReplayViewer.ts
@@ -0,0 +1,10 @@
+///<reference path="../js/facepunch.webgame.d.ts"/>
+///<reference path="../js/sourceutils.d.ts"/>
+
+import WebGame = Facepunch.WebGame;
+
+class ReplayViewer extends SourceUtils.MapViewer {
+    protected onInitialize(): void {
+        this.useDefaultCameraControl = false;
+    }
+}
